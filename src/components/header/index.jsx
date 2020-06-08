9a0c1e747c68f784d50200bd099d88988c23f59a
Felix Battig <fbattig@kreonovo.ca>
2020-06-08 08:34:28 -0400

    added Redux, and implemented userReducer ans userActions to header and App components

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,31 +2,37 @@ import React from 'react';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 import './styles.scss';
 
-export default function index({ currentUser }) {
-	return (
-		<div className="header">
-			<Link to="/" className="logo-container">
-				<Logo className="logo" />
-			</Link>
-			<div className="options">
-				<Link className="option" to="/shop">
-					SHOP
+const index =({currentUser})=> {
+		return (
+			<div className="header">
+				<Link to="/" className="logo-container">
+					<Logo className="logo" />
 				</Link>
-				<Link className="option" to="/shop">
-					CONTACT
-				</Link>
-				{currentUser ? (
-					<div className="option" onClick={() => auth.signOut()}>
-						SIGN OUT
-					</div>
-				) : (
-					<Link to="/signIn" className="option">
-						SIGN IN
+				<div className="options">
+					<Link className="option" to="/shop">
+						SHOP
 					</Link>
-				)}
+					<Link className="option" to="/shop">
+						CONTACT
+					</Link>
+					{currentUser ? (
+						<div className="option" onClick={() => auth.signOut()}>
+							SIGN OUT
+						</div>
+					) : (
+						<Link to="/signIn" className="option">
+							SIGN IN
+						</Link>
+					)}
+				</div>
 			</div>
-		</div>
-	);
-}
+		);
+	}
+const mapStateToProps = (state) => ({
+	currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(index);
