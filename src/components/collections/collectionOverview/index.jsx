de113ec56a4e943cf958955b7883ee972750ad1d
Felix Battig <fbattig@kreonovo.ca>
2020-06-15 07:05:38 -0400

    fixed collectionIverview glitch

--- src/components/collections/collectionOverview/index.jsx
+++ src/components/collections/collectionOverview/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../collection-preview'
-import { selectCollections } from '../../../redux/shop/shopSelector'
+import CollectionPreview from '../collection-preview';
+import { selectCollectionsForPreview  } from '../../../redux/shop/shopSelector';
 import './styles.scss';
 
-const index =({collections}) => {
-  return <div className='collections-overview'>
-    	{collections.map(({ id, ...otherCollectionProps }) => {
-				return <CollectionPreview key={id} {...otherCollectionProps} />;
-			})}
-  </div>;
-}
+const index = ({ collections }) => (
+	<div className="collections-overview">
+		{collections.map(({ id, ...otherCollectionProps }) => (
+			<CollectionPreview key={id} {...otherCollectionProps} />
+		))}
+	</div>
+);
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
+	collections: selectCollectionsForPreview ,
+});
 export default connect(mapStateToProps)(index);
